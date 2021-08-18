374a73ad1b434fd25bcb2b3ca0086361e084d4e5
YUDESHUIQINLEI <13081850733@163.com>
2021-08-18 21:20:58 +0800

    add

---
+++ 宽度优先遍历/_257_二叉树的所有路径.java
@@ -0,0 +1,12 @@
+package 宽度优先遍历;
+
+public class _257_二叉树的所有路径 {
+    /**
+     * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+
+    }
+}
